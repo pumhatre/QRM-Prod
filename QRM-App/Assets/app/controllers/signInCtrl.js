35c29abb2ebd58f85b8d7ca926d42e229b43f09a
hariksubramani <harishprofm@gmail.com>
2017-09-19 16:51:40 +0530

    Login Page Changed

--- QRM-App/Assets/app/controllers/signInCtrl.js
+++ QRM-App/Assets/app/controllers/signInCtrl.js
@@ -5,14 +5,19 @@
             var apiUrl='http://localhost:60038/api/oauth/login';
             var params = "grant_type=password&username=" + $scope.username + "&password=" + $scope.password;
             $http.post(apiUrl, params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
-                .then(function (successReponse) {                  
+                .then(function (successReponse) {                   
                     $http.defaults.headers.common.Authorization = "Bearer " + successReponse.data.access_token;
                     $cookieStore.put('_Token', successReponse.data.access_token);
                     $cookieStore.put('_UserId', successReponse.data.UserId);
                     $cookieStore.put('_UserName', successReponse.data.UserName);
                     $cookieStore.put('_RoleName', successReponse.data.RoleName);
                     window.location.href = '/#!home';
-
+                    if (successReponse.data.RoleName == "SuperUser")
+                    {
+                        $scope.IsSuperUser = true;
+                    } else {
+                        $scope.IsSuperUser = false;
+                    }
 
                 }, function (errorReponse) {
                     $scope.message = errorReponse.data.error_description.replace(/["']{1}/gi, "");
