cd026ed018422d7e473d57c2f7d136e7f345ed34
hariksubramani <harishprofm@gmail.com>
2017-09-19 12:36:34 +0530

    Signin Module checkin

--- QRM-App/Assets/app/controllers/signInCtrl.js
+++ QRM-App/Assets/app/controllers/signInCtrl.js
@@ -1,13 +1,11 @@
 ﻿angular.module('signIn', ['ngCookies'])
     .controller('signInCtrl', ['$scope', '$rootScope', '$http', '$cookies', '$cookieStore', '$location',  function ($scope, $rootScope, $http, $cookies, $cookieStore, $location) {
-        $scope.signIn = function () {
-            debugger;
+        $scope.signIn = function () {           
             $scope.showMessage = false;
             var apiUrl='http://localhost:60038/api/oauth/login';
             var params = "grant_type=password&username=" + $scope.username + "&password=" + $scope.password;
             $http.post(apiUrl, params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
-                .then(function (successReponse) {
-                    debugger;
+                .then(function (successReponse) {                  
                     $http.defaults.headers.common.Authorization = "Bearer " + successReponse.data.access_token;
                     $cookieStore.put('_Token', successReponse.data.access_token);
                     $cookieStore.put('_UserId', successReponse.data.UserId);
