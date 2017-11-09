44385ec940b2884c8a39ba776e6f96708b4a9c47
Murali <chkrish.9@dummy.com>
2017-11-09 17:48:40 +0530

    Upload module - converting excel rows to viewmodel

--- QRM-App/Assets/app/services/uploadService.js
+++ QRM-App/Assets/app/services/uploadService.js
@@ -1,7 +1,91 @@
 ﻿app.service('uploadService', ['$http', 'config', function ($http, config) {
     this.UploadFile = function (files, saveData) {
         let webWorker = new Worker(config.baseUrl+'Assets/app/workers/uploadWorker.js');
-        var message = { files: files,config:config };
+        var message = {
+            files: files,
+            config: config,
+            neededSheets: ["Effort Data", "Defect Data", "Testing Data"],
+            neededSheetsMandatory:{
+                "Effort Data": "Object/ Component ID",
+                "Defect Data": "Defect ID",
+                "Testing Data": "Testing Phase"
+            },
+            neededSheetsViewModels: {
+                "Effort Data": {
+                    "Object/ Component ID": "Object/ Component ID",
+                    "Component Type": "Component Type",
+                    "Object/ Widget Type": "Object/ Widget Type",
+                    "Complexity": "Complexity",
+                    "Task Type": "Task Type",
+                    "Baselined Effort(hrs)": "Baselined Effort(hrs)",
+                    "Actual Effort":"Actual Effort",
+                    "Status":"Status",
+                    "CMMI Rollup":"CMMI Rollup",
+                    "SEQ":"SEQ",
+                    "Scheduled Start Date":"Scheduled Start Date",
+                    "Scheduled End Date":"Scheduled End Date",
+                    "Actual Start Date":"Actual Start Date",
+                    "Actual End Date":"Actual End Date",
+                    "Project":"Project",
+                    "Release":"Release",
+                    "Module":"Module",
+                    "Object/ Component Name":"Object/ Component Name",
+                    "Review Type":"Review Type",
+                    "Remarks": "Remarks"
+                },
+                "Defect Data": {
+                    "Defect ID":"Defect ID",
+                    "Object/ Widget ID":"Object/ Widget ID",
+                    "Defect Detected Stage":"Defect Detected Stage",
+                    "Reported Date":"Reported Date",
+                    "Reported By":"Reported By",
+                    "Defect Description":"Defect Description",
+                    "Status":"Status",
+                    "Defect Injected Stage":"Defect Injected Stage",
+                    "Expected Detection Phase":"Expected Detection Phase",
+                    "Defect Type":"Defect Type",
+                    "Cause":"Cause",
+                    "Review Type":"Review Type",
+                    "Defect Severity":"Defect Severity",
+                    "Fixed On Date":"Fixed On Date",
+                    "Remarks/ Notes": "Remarks/ Notes"
+
+                },
+                "Testing Data": {
+                    "Testing Phase":"Testing Phase",
+                    "Testing Type":"Testing Type",
+                    "Module":"Module",
+                    "Release/ Iteration":"Release/ Iteration",
+                    "Planned No. of Test cases Designed":"Planned No. of Test cases Designed",
+                    "Actual No. of Test cases Designed":"Actual No. of Test cases Designed",
+                    "No. of Test Case Review Comments":"No. of Test Case Review Comments",
+                    "Planned Start Date":"Planned Start Date",
+                    "Plannned End Date":"Plannned End Date",
+                    "Actual Start Date":"Actual Start Date",
+                    "Actual End Date":"Actual End Date",
+                    "Test Design Status":"Test Design Status",
+                    "Manual execution or Automated?":"Manual execution or Automated?",
+                    "Test Case Preparation":"Test Case Preparation",
+                    "Test Case Review":"Test Case Review",
+                    "Test Case Rework":"Test Case Rework",
+                    "Test Case Preparation":"Test Case Preparation",
+                    "Test Case Review":"Test Case Review",
+                    "Test Case Rework":"Test Case Rework",
+                    "TC1-No. of Test cases Planned for execution":"TC1-No. of Test cases Planned for execution",
+                    "TC1-Planned Effort for Test Execution":"TC1-Planned Effort for Test Execution",
+                    "TC1 Execution Status":"TC1 Execution Status",
+                    "TC1-No. of Test cases Executed":"TC1-No. of Test cases Executed",
+                    "TC1-Actual Effort for Test Execution":"TC1-Actual Effort for Test Execution",
+                    "TC1-Total Test Cases passed":"TC1-Total Test Cases passed",
+                    "TC1-Defects found":"TC1-Defects found",
+                    "TC1-Defects Rejected":"TC1-Defects Rejected",
+                    "Test Design Productivity":"Test Design Productivity",
+                    "TC1-Test Exection Productivity":"TC1-Test Exection Productivity",
+                    "TC1-Defect Rejection %":"TC1-Defect Rejection %",
+                    "Month-Test Case Creation End Date": "Month-Test Case Creation End Date"
+                }
+            }
+        };
         webWorker.postMessage(message);
         webWorker.onmessage = function (e) {
             saveData(e.data);
--- QRM-App/Assets/app/workers/uploadWorker.js
+++ QRM-App/Assets/app/workers/uploadWorker.js
@@ -2,7 +2,12 @@
 this.onmessage = function receiveMessage(message) {
     importScripts(message.data.config.baseUrl + 'Assets/jsxlsx/xlsx.js');
     importScripts(message.data.config.baseUrl + 'Assets/jsxlsx/jszip.js');
+    importScripts(message.data.config.baseUrl + 'Assets/jsxlsx/lodash.js');
     var files = message.data.files;
+    var needsheets = message.data.neededSheets;
+    var neededSheetsViewModels = message.data.neededSheetsViewModels;
+    var neededSheetsMandatory=message.data.neededSheetsMandatory;
+
     var rABS = typeof FileReader !== "undefined" && typeof FileReader.prototype !== "undefined" && typeof FileReader.prototype.readAsBinaryString !== "undefined";
     var i, f;
     //Loop through files
@@ -18,21 +23,30 @@ this.onmessage = function receiveMessage(message) {
             var arr = rABS ? data : btoa(fixdata(data));
             //reading data from excel
             var workbook = XLSX.read(arr, { type: rABS ? 'binary' : 'base64' });
+           
+
             //Getting the sheet names
             var sheet_name_list = workbook.SheetNames;
             var count = 0;
             sheet_name_list.forEach(function (y) { /* iterate through sheets */
                 //Convert the cell value to Json
-                var needsheets = ["Effort Data", "Defect Data", "Testing Data"];
                 if (needsheets.indexOf(y) > -1) {
-                    var roa = XLSX.utils.sheet_to_json(workbook.Sheets[y], {header:"A"});
-                    if (roa.length > 0) {
-                        //console.log(roa);
-                        var sheetObj = {};
-                        sheetObj[y] = roa;
-                        result[count] = sheetObj;
-                        count++;
+                    //var roa = XLSX.utils.sheet_to_json(workbook.Sheets[y]);
+                    //if (roa.length > 0) {
+                    //    //console.log(roa);
+                    //    var sheetObj = {};
+                    //    sheetObj[y] = roa;
+                    //    result[count] = sheetObj;
+                    //    count++;
+                    //}
+                    let s = findSheet(workbook, y);
+                    let t = findTable(s.sheet, s.range, neededSheetsViewModels[y]);
+                    if (t.firstRow === null) {
+                        return null;
                     }
+                    const tdata = readTable(s.sheet, s.range, t.columns, t.firstRow, neededSheetsMandatory[y], (row) => false);
+                    result[count] = tdata;
+                    count++;
                 }
             });
             self.postMessage(result);
@@ -44,4 +58,82 @@ var fixdata = function (data) {
     for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)));
     o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)));
     return o;
+}
+
+var findSheet = function (workbook, sheetName) {
+    const dc = XLSX.utils.decode_cell;
+    let sheet = workbook.Sheets[sheetName],
+        range = {min: {r: 0, c: 0}, max: {r: 0, c: 0}};
+
+    if(!sheet) {
+        return { sheet: null, range: null };
+    }
+
+    // find size of the sheet
+    let ref = sheet['!ref'];
+
+    if(!ref && ref.indexOf(':') === -1) {
+        throw new Error("Malformed workbook - no !ref property");
+    }
+
+    range.min = dc(ref.split(':')[0]);
+    range.max = dc(ref.split(':')[1]);
+
+    return { sheet, range };
+}
+
+var findTable = function (sheet, range, colMap) {
+    const ec = (r, c) => { return XLSX.utils.encode_cell({ r: r, c: c }); };
+    let firstRow = null,
+        colsToFind = _.keysIn(colMap).length,
+
+        // colmap lowercase title -> prop
+        colLookup = _.reduce(colMap, (m, v, k) => { m[_.isString(v)? v.toLowerCase() : v] = k; return m; }, {}),
+
+        // colmap props -> 0-indexed column
+        columns = _.reduce(colMap, (m, v, k) => { m[k] = null; return m; }, {});
+
+    // Look for header row and extract columns
+    for(let r = range.min.r; r <= range.max.r - 1; ++r) {
+        let colsFound = 0;
+
+        for(let c = range.min.c; c <= range.max.c; ++c) {
+            let cell = sheet[ec(r, c)];
+
+            if(cell && cell.v !== undefined) {
+                let prop = colLookup[cell.t === 's'? cell.v.toLowerCase() : cell.v];
+                if(prop) {
+                    columns[prop] = c;
+                    ++colsFound;
+                }
+            }
+        }
+
+        if(colsFound === colsToFind) {
+            firstRow = r + 1;
+            break;
+        }
+    }
+
+    return { columns, firstRow };
+}
+var readTable = function (sheet, range, columns, firstRow,neededSheetsMandatory, stop) {
+    const ec = (r, c) => { return XLSX.utils.encode_cell({ r: r, c: c }); };
+    let data = [];
+
+    for(let r = firstRow; r <= range.max.r; ++r) {
+        let row = _.reduce(columns, (m, c, k) => {
+            let cell = sheet[ec(r, c)];
+            m[k] = cell? cell.v : null;
+            return m;
+        }, {});
+
+        if(stop && stop(row)) {
+            break;
+        }
+        if (row[neededSheetsMandatory] != null)
+            data.push(row);
+    }
+
+    return data;
 }
