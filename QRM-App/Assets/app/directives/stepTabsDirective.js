a5f0c01726fbff14d15dfecaaf05df73b4a928b0
Murali <chkrish.9@dummy.com>
2017-11-12 01:25:53 +0530

    Steps tabs directive UI changes

--- QRM-App/Assets/app/directives/stepTabsDirective.js
+++ QRM-App/Assets/app/directives/stepTabsDirective.js
@@ -8,25 +8,25 @@ function StepTabs(){
             template: '<div class="stepwizard col-md-offset-3">' +
                         ' <div class="stepwizard-row setup-panel">' +
                             '<div class="stepwizard-step" ng-repeat="option in options" style="width:{{width}}%">' +
-                                '<button id="{{$index}}" href="#{{option.Id}}"type="button" ng-class="{' +'\'btn-black\'' + ':{{$index==0}} ,' + '\'btn-default\'' + ':{{$index!=0}}}"class="btn btn-circle" ng-disabled="{{$index!=0}}" ng-click="buttonClick($index)">{{$index+1}}</button>' +
+                                '<button id="{{$index}}" href="#{{option.Id}}"type="button" ng-class="{' +'\'btn-black active\'' + ':{{$index==0}} ,' + '\'btn-grey\'' + ':{{$index!=0}}}"class="btn btn-circle" ng-disabled="{{$index!=0}}" ng-click="buttonClick($index)">{{$index+1}}</button>' +
                                 '<p>{{option.DisplayHeaderName}}</p>' +
                                 '<p>{{option.DisplaySubHeaderName}}</p>' +
                             '</div>'+
                           '</div>'+
                       '</div>',
             link: function ($scope, element, attributes) {
-                $scope.width = (100 / $scope.options.length);
+              $scope.width = (100 / $scope.options.length);
               var navListItems = $('div.setup-panel div button'),
               allWells = $('.setup-content'),
               allNextBtn = $('.nextBtn');
               allPreviousBtn = $('.previousBtn');
               allWells.hide();
-                $(allWells[0]).show();
+              $(allWells[0]).show();
                 $scope.buttonClick=function (e) {
                     var $target = $($("#" + e).attr("href")),
                         $item = $("#" + e);
-                        $('div.setup-panel div button').removeClass('btn-black').addClass('btn-default');
-                        $item.removeClass('btn-default').addClass('btn-black');
+                        $('div.setup-panel div button').removeClass('btn-black').addClass('btn-grey');
+                        $item.removeClass('btn-grey btn-green completed').addClass('btn-black active');
                         allWells.hide();
                         $target.show();
                         $target.find('input:eq(0)').focus();
@@ -36,36 +36,27 @@ function StepTabs(){
                     var curStep = $(this).closest(".setup-content"),
                         curStepBtn = curStep.attr("id"),
                         nextStepWizard = $('div.setup-panel div button[href="#' + curStepBtn + '"]').parent().next().children("button"),
-                        curInputs = curStep.find("input[type='text'],input[type='url']"),
-                        isValid = true;
+                        curInputs = curStep.find("input[type='text'],input[type='url']");
                     var index=_.findIndex($scope.options,function(step){
                         return step.Id == curStepBtn
                     });
                     if ($scope.options[index].NextClickCallback) {
                         $scope.options[index].NextClickCallback(e);
                     }
-                    $(".form-group").removeClass("has-error");
-                    for (var i = 0; i < curInputs.length; i++) {
-                        if (!curInputs[i].validity.valid) {
-                            isValid = false;
-                            $(curInputs[i]).closest(".form-group").addClass("has-error");
-                        }
-                    }
 
-                    if (isValid) {
-                        $('div.setup-panel div button[href="#' + curStepBtn + '"]').removeClass('btn-black').addClass("btn-success");
-                        $('div.setup-panel div button[href="#' + curStepBtn + '"]').attr('disabled', 'disabled');
-                        nextStepWizard.removeAttr('disabled').trigger('click');
-                    }
+                    $('div.setup-panel div button[href="#' + curStepBtn + '"]').removeClass('btn-black active').addClass("btn-green completed");
+                    $('div.setup-panel div button[href="#' + curStepBtn + '"]').attr('disabled', 'disabled');
+                    nextStepWizard.removeAttr('disabled').trigger('click');
+                    
                 });
                 allPreviousBtn.click(function () {
                     var curStep = $(this).closest(".setup-content"),
                         curStepBtn = curStep.attr("id"),
                         nextStepWizard = $('div.setup-panel div button[href="#' + curStepBtn + '"]').parent().prev().children("button"),
                         curInputs = curStep.find("input[type='text'],input[type='url']");
-                        $('div.setup-panel div button[href="#' + curStepBtn + '"]').removeClass('btn-black').addClass("btn-success");
-                        $('div.setup-panel div button[href="#' + curStepBtn + '"]').attr('disabled', 'disabled');
-                        nextStepWizard.removeAttr('disabled').trigger('click');
+                    $('div.setup-panel div button[href="#' + curStepBtn + '"]').removeClass('btn-black active').addClass("btn-grey");
+                    $('div.setup-panel div button[href="#' + curStepBtn + '"]').attr('disabled', 'disabled');
+                    nextStepWizard.removeAttr('disabled').trigger('click');
                 });
            
             }
