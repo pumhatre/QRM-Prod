02ad9f2cbb8997455bd02f0d5f4e66e378d7932f
Vinay <vinasingh@deloitte.com>
2018-06-05 19:22:34 +0530

    new chart data model added

---
+++ QRMService/Models/ChartDataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRMService.Models
+{
+    public class ChartDataModel
+    {
+        public List<string> labels { get; set; }
+        public List<Dataset> datasets { get; set; }
+    }
+
+    public class Dataset
+    {
+        public List<int> data { get; set; }
+        public string label { get; set; }
+        public string borderColor { get; set; }
+        public bool fill { get; set; }
+        public List<string> backgroundColor { get; set; }
+        public string pointBorderColor { get; set; }
+        public string pointBackgroundColor { get; set; }
+
+    }
+
+}
