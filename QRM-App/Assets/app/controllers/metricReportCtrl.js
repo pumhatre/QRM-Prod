6a53639f6ceff369aa1d8f77c0c36ab3a6da7d50
Bamne, Rushikesh (US - Mumbai) <rbamne@deloitte.com>
2018-06-07 15:02:50 +0530

    For Chart and Metric Report

---
+++ QRM-App/Assets/app/controllers/metricReportCtrl.js
@@ -0,0 +1,47 @@
+﻿/*
+* Module:Chart
+* Description:This controller will be used for user specfic information Home
+*/
+"use strict";
+angular.module('metricReport', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.saveState', 'ui.grid.selection', 'ui.grid.cellNav', 'ui.grid.resizeColumns', 'ui.grid.moveColumns', 'ui.grid.pinning', 'ui.bootstrap', 'ui.grid.autoResize', 'chart.js'])
+    .controller('metricReportCtrl', ['$scope', 'homeService', 'healthReportService', 'chartService', '$cookies', '$cookieStore', 'config', 'uiGridConstants', '$templateCache', 'projectReleaseService', 'metricsAssociationService', function ($scope, homeService, healthReportService, chartService, $cookies, $cookieStore, config, uiGridConstants, $templateCache, projectReleaseService, metricsAssociationService) {
+        $scope.projectsDropdown = [];
+        $scope.projectsReleases = [];
+        $scope.selectedProjectReleaseDropdown = '';
+
+
+        // function to load projects dropdown
+        $scope.LoadProjectsDropDown = function () {
+            projectReleaseService.GetProjectsLists(config)
+                .then(function (successResponse) {
+                    $scope.projectsDropdown = successResponse.data;
+                }, function (errorResponse) {
+
+                });
+        }
+
+        $scope.getProjectReleases = function (projectId) {
+            metricsAssociationService.getReleaseList(config, projectId)
+                .then(function (successResponse) {
+                    $scope.releaseDropdown = successResponse.data;
+                }, function (errorResponse) {
+
+                });
+        }
+
+        $scope.getSelectedProjectMonth = function (projectId, releaseId) {
+            if (projectId != null && releaseId != null) {
+                metricsAssociationService.getSelectedProjectMonth(config, projectId, releaseId)
+                    .then(function (successResponse) {
+                        $scope.selectedMonth = successResponse.data;
+                    }, function (errorResponse) {
+
+                    });
+            }
+        }
+
+
+
+        // load projects dropdown on load
+        $scope.LoadProjectsDropDown();
+    }]);
