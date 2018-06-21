f6b16a976111d978699bd470fb07b4c4c72989de
Vinay <vinasingh@deloitte.com>
2018-06-21 16:22:08 +0530

    my saved report controller added

---
+++ QRMService/Controllers/SaveReportsController.cs
@@ -0,0 +1,71 @@
+﻿using QRMService.Models;
+using QRMService.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QRMService.Controllers
+{
+    public class SaveReportsController : ApiController
+    {
+        /// <summary>
+        /// Saves the report.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult SaveReport(MySavedReportsRequestModel request)
+        {
+            try
+            {
+                var isSuccess = MySavedReportRepository.SaveReport(request.UserId, request.ProjectId, request.ProjectReleaseID, request.ReportType, request.ReportName);
+                return Ok(new { Success = isSuccess });
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        /// <summary>
+        /// Deletes my report.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult DeleteMyReport(MySavedReportsRequestModel request)
+        {
+            try
+            {
+                var isSuccess = MySavedReportRepository.DeleteMySavedReport(request.UserReportAssociationID);
+                return Ok(new { Success = isSuccess });
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+
+        /// <summary>
+        /// Gets my saved reports.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <returns></returns>
+        [HttpPost]
+        public IHttpActionResult GetMySavedReports(MySavedReportsRequestModel request)
+        {
+            try
+            {
+                var data = MySavedReportRepository.GetMySavedReports(request.UserId);
+                return Ok(data);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+        }
+    }
+}
---
+++ QRMService/Models/MySavedReportsRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QRMService.Models
+{
+    public class MySavedReportsRequestModel
+    {
+        public int UserId { get; set; }
+        public int ProjectReleaseID { get; set; }
+        public int ProjectId { get; set; }
+        public string ReportType { get; set; }
+        public string ReportName { get; set; }
+        public int UserReportAssociationID { get; set; }
+    }
+}
