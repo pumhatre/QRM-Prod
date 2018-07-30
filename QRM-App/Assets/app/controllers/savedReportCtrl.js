6d8744380cc0a28f87a9c7177c73ab2adb84ebdb
annpriya <annupriya0304@gmail.com>
2018-07-30 19:18:35 +0530

    changes for delete saved reports

--- QRM-App/Assets/app/controllers/savedReportCtrl.js
+++ QRM-App/Assets/app/controllers/savedReportCtrl.js
@@ -1,6 +1,8 @@
 ﻿"use strict";
 angular.module('savedReport', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.saveState', 'ui.grid.selection', 'ui.grid.cellNav', 'ui.grid.resizeColumns', 'ui.grid.moveColumns', 'ui.grid.pinning', 'ui.bootstrap', 'ui.grid.autoResize', 'chart.js'])
-    .controller('savedReportCtrl', ['$scope', '$cookies', '$cookieStore', 'config', 'uiGridConstants', '$templateCache', 'mySavedReportService', '$rootScope', function ($scope, $cookies, $cookieStore, config, uiGridConstants, $templateCache, mySavedReportService, $rootScope) {
+    .controller('savedReportCtrl', ['$scope', '$confirm', '$cookies', '$cookieStore', 'config', 'uiGridConstants', '$templateCache', 'mySavedReportService', '$rootScope', function ($scope,$confirm, $cookies, $cookieStore, config, uiGridConstants, $templateCache, mySavedReportService, $rootScope) {
+
+        $scope.alerts = [];
         var tmpl = '<div style="padding: 5px;" ng-if="!row.entity.editable">{{COL_FIELD}}</div><div ng-if="row.entity.editable"><input ng-model="MODEL_COL_FIELD"</div>';
         $scope.savedReportsGrid = {
             enableSorting: false,
@@ -18,7 +20,7 @@ angular.module('savedReport', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.saveS
                 {
                     name: '', field: 'edit', enableFiltering: false, enableSorting: false, enableColumnMenu: false, width: '12%',
                     cellTemplate: '<div style="text-align: center; padding-top: 5px; padding-bottom: 5px;"><button ng-show="!row.entity.editable" ng-click="grid.appScope.redirectToChart(row.entity.ProjectId,row.entity.ProjectReleaseID,row.entity.ReportType)" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-th-list"></i> View</button>' +  //View Button
-                        '<button ng-show="!row.entity.editable" ng-click="grid.appScope.deleteRow(row.entity)" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete</button>' + //Delete Button
+                        '<button ng-show="!row.entity.editable" ng-click="grid.appScope.deleteRow(row.entity.UserReportAssociationID)" class="btn btn-danger btn-xs"><i class="fa fa-trash-o"></i> Delete</button>' + //Delete Button
                         '</div>'
                 }
             ],
@@ -50,4 +52,27 @@ angular.module('savedReport', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.saveS
             window.location.href = '/app/qrm/#!Chart?ref=v';
         }
 
+        $scope.deleteRow = function (reportId) {
+            $confirm({ text: 'Are you sure you want to delete this record?' })
+                .then(function () {
+
+                    mySavedReportService.DeleteMyReport(config, reportId)
+                        .then(function (response) {
+                            if (response.data.Success) {
+                                $scope.getSavedReports();
+                                //Display Successfull message after save
+                                $scope.alerts.push({
+                                    msg: 'Report deleted successfully',
+                                    type: 'success'
+                                });
+                            }
+                        }, function (errorResponse) {
+                            $scope.alerts.push({
+                                msg: error.data.ResponseMessage,
+                                type: 'danger'
+                            });
+                        });
+                });
+
+        }
     }]);
