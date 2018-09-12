af06d3aaeac1761fa9abf4f4c950d80048ce83d6
annpriya <annupriya0304@gmail.com>
2018-09-12 19:55:18 +0530

    productivity

--- QRM-App/Assets/app/controllers/metricReportCtrl.js
+++ QRM-App/Assets/app/controllers/metricReportCtrl.js
@@ -631,17 +631,17 @@ angular.module('metricReport', ['ngAnimate', 'ngTouch', 'ui.grid', 'ui.grid.save
 
 
             columnDefs: [
-                { name: 'Type', superCol: 'type', displayName: '', cellTemplate: '<div style="padding-top: 75px; padding-left:10px" class="ui-grid-cell-contents wrap" title="TOOLTIP" ng-style="{ height:20*row.entity.spanEffort + \'px\', width:15+\'%\', position:\'absolute\', display:row.entity.spanEffort==0?\'none\':\'block\', background: \'#f3f3f3\'}" ><b>{{COL_FIELD}}</b></div>'},
-                { name: 'SubType', superCol: 'subType', displayName: ' '},
-                { name: 'ProjectPerformance', displayName: 'Project Performance', superCol: 'java' },
-                { name: 'USL', displayName: 'USLJava', superCol: 'java' },
-                { name: 'LSL', displayName: 'LSLJava', superCol: 'java' },
-                { name: 'ProjectPerformanceDOTNET', displayName: 'Project Performance D', superCol: 'dotNet' },
-                { name: 'USLDOTNET', displayName: 'USLDot', superCol: 'dotNet' },
-                { name: 'LSLDONET', displayName: 'LSLDot', superCol: 'dotNet' },
-                { name: 'ProjectPerformanceOther', displayName: 'Project Performance O', superCol: 'other' },
-                { name: 'USLOther', displayName: 'USLOther', superCol: 'other' },
-                { name: 'LSLOther', displayName: 'LSLOther', superCol: 'other' }
+                { name: 'Type', superCol: 'type', displayName: '', width: '14%',cellTemplate: '<div style="padding-top: 75px; padding-left:10px" class="ui-grid-cell-contents wrap" title="TOOLTIP" ng-style="{ height:20*row.entity.spanEffort + \'px\', width:14+\'%\', position:\'absolute\', display:row.entity.spanEffort==0?\'none\':\'block\', background: \'#f3f3f3\'}" ><b>{{COL_FIELD}}</b></div>' },
+                { name: 'SubType', superCol: 'subType', displayName: ' ', width: '14%' },
+                { name: 'ProjectPerformance', displayName: 'Project Performance', superCol: 'java', width: '12%' },
+                { name: 'USL', displayName: 'USL', superCol: 'java', width: '6%' },
+                { name: 'LSL', displayName: 'LSL', superCol: 'java', width: '6%' },
+                { name: 'ProjectPerformanceDOTNET', displayName: 'Project Performance ', superCol: 'dotNet', width: '11%' },
+                { name: 'USLDOTNET', displayName: 'USL ', superCol: 'dotNet', width: '6%' },
+                { name: 'LSLDOTNET ', displayName: 'LSL ', superCol: 'dotNet', width: '6%' },
+                { name: 'ProjectPerformanceOther', displayName: 'Project Performance  ', superCol: 'other', width: '11%' },
+                { name: 'USLOther', displayName: 'USL  ', superCol: 'other', width: '6%'   },
+                { name: 'LSLOther', displayName: 'LSL  ', superCol: 'other', width: '6%' }
 
             ],
             enableGridMenu: true,
